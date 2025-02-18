86b39526b09a75bac22193fd5edd5f42c1e5e3c1
joybully <joybully@khu.ac.kr>
2025-02-19 07:03:43 +0900

    update: Member entity와 main application의 필요없는 어노테이션 삭제

--- src/main/java/com/example/eroom/domain/entity/Member.java
+++ src/main/java/com/example/eroom/domain/entity/Member.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
+//@EntityListeners(AuditingEntityListener.class)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
