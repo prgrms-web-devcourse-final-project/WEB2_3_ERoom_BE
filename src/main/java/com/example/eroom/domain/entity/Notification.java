2cb55b820f5be13af88b938e7c79fab72f35e3ff
suho <yousuho1232@naver.com>
2025-02-19 11:31:59 +0900

    feat : 프로젝트 생성 시 알림 기능 구현

---
+++ src/main/java/com/example/eroom/domain/entity/Notification.java
@@ -0,0 +1,39 @@
+package com.example.eroom.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member recipient; // 알림을 받는 유저
+
+    private String message;
+    private boolean isRead;
+
+    private LocalDateTime createdAt;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType type; // PROJECT_INVITE, PROJECT_EXIT
+
+    // 연관된 프로젝트
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    // 관련 엔티티의 ID만 저장하는 방식도 괜찮을듯
+    //private Long referenceId;
+}
---
+++ src/main/java/com/example/eroom/domain/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package com.example.eroom.domain.entity;
+
+public enum NotificationType {
+    PROJECT_INVITE,
+    PROJECT_EXIT,
+    TASK_ASSIGN,
+    CHAT_MESSAGE
+}
