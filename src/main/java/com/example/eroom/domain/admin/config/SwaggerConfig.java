6afbaeb0af0ca3f823ba6771b15b63cb276e0f5a
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-02-19 10:56:38 +0900

    로컬 변경 사항 커밋

--- src/main/java/com/example/eroom/domain/admin/config/SwaggerConfig.java
+++ src/main/java/com/example/eroom/domain/admin/config/SwaggerConfig.java
@@ -1,20 +1,19 @@
 package com.example.eroom.domain.admin.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "Eroom API", version = "v1", description = "Eroom API Documentation"))
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .group("Eroom API")
-                .packagesToScan("com.example.eroom")
-                .build();
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(info);
     }
+
+    Info info = new Info().title("Swagger Test").version("0.0.1").description(
+            "<h3>E:room Swagger test</h3>");
 }
