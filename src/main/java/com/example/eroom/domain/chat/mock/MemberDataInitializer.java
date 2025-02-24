e5ce11edb8477a938fe94793eadd4bd697c7866e
joybully <joybully@khu.ac.kr>
2025-02-24 12:26:07 +0900

    refactor: 목업데이터 복원

--- src/main/java/com/example/eroom/domain/chat/mock/MemberDataInitializer.java
+++ src/main/java/com/example/eroom/domain/chat/mock/MemberDataInitializer.java
@@ -16,23 +16,23 @@ public class MemberDataInitializer implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Member member1 = new Member();
-        member1.setUsername("member5");
-        member1.setEmail("qwerty5@gmail.com");
+        member1.setUsername("member1");
+        member1.setEmail("qwerty1@gmail.com");
         member1.setPassword("1234");
 
         Member member2 = new Member();
-        member2.setUsername("member6");
-        member2.setEmail("qwerty6@gmail.com");
+        member2.setUsername("member2");
+        member2.setEmail("qwerty2@gmail.com");
         member2.setPassword("1234");
 
         Member member3 = new Member();
-        member3.setUsername("member7");
-        member3.setEmail("qwerty7@gmail.com");
+        member3.setUsername("member3");
+        member3.setEmail("qwerty3@gmail.com");
         member3.setPassword("1234");
 
         Member member4 = new Member();
-        member4.setUsername("member8");
-        member4.setEmail("qwerty8@gmail.com");
+        member4.setUsername("member4");
+        member4.setEmail("qwerty4@gmail.com");
         member4.setPassword("1234");
 
         memberRepository.save(member1);
