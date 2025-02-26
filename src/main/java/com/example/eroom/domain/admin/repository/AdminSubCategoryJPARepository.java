3f28a76f2a6145cd8c0b7a1ae74ca46e7c145b33
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-02-27 01:14:49 +0900

    feat: 관리자 ) 서브 카테고리 관리 기능 구현

--- src/main/java/com/example/eroom/domain/admin/repository/AdminSubCategoryJPARepository.java
+++ src/main/java/com/example/eroom/domain/admin/repository/AdminSubCategoryJPARepository.java
@@ -1,9 +1,13 @@
 package com.example.eroom.domain.admin.repository;
 
+import com.example.eroom.domain.entity.Category;
 import com.example.eroom.domain.entity.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AdminSubCategoryJPARepository extends JpaRepository<SubCategory, Long> {
+    List<SubCategory> findByCategory(Category category);
 }
