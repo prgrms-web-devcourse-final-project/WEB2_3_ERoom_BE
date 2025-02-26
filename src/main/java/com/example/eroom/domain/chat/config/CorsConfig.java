b06abbd9b619a5e8733e0a8cf4b1ee9a2d2e8866
suho <yousuho1232@naver.com>
2025-02-26 12:22:59 +0900

    feat : CorsConfig 설정 추가

---
+++ src/main/java/com/example/eroom/domain/chat/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.eroom.domain.chat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173") // 허용할 도메인
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
