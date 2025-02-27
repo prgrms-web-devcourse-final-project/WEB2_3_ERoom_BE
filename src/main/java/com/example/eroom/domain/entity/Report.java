ded09cc8ed35b8732916bdce1329d8b395e6e6de
joybully <joybully@khu.ac.kr>
2025-02-27 16:29:33 +0900

    feat: report entity생성

---
+++ src/main/java/com/example/eroom/domain/entity/Report.java
@@ -0,0 +1,36 @@
+package com.example.eroom.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    @Column(columnDefinition = "LONGTEXT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "chatroom_id")
+    private ChatRoom chatRoom;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @Enumerated(EnumType.STRING)
+    private DeleteStatus deleteStatus; // ACTIVE, DELETED
+
+}
