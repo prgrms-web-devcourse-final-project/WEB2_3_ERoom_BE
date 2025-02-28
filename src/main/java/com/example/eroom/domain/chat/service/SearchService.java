1403549c50a91a3e5b8723befb82167000d76691
suho <yousuho1232@naver.com>
2025-02-28 10:01:43 +0900

    feat : 멤버 검색 응답 DTO, 프로젝트 검색 응답 DTO에 DeleteStatus 추가

--- src/main/java/com/example/eroom/domain/chat/service/SearchService.java
+++ src/main/java/com/example/eroom/domain/chat/service/SearchService.java
@@ -1,7 +1,6 @@
 package com.example.eroom.domain.chat.service;
 
 import com.example.eroom.domain.chat.dto.response.MemberSearchResponseDTO;
-import com.example.eroom.domain.chat.dto.response.ProjectResponseDTO;
 import com.example.eroom.domain.chat.dto.response.ProjectSearchResponseDTO;
 import com.example.eroom.domain.chat.repository.MemberRepository;
 import com.example.eroom.domain.chat.repository.ProjectRepository;
@@ -29,7 +28,8 @@ public class SearchService {
                         member.getId(),
                         member.getUsername(),
                         member.getEmail(),
-                        member.getProfile()
+                        member.getProfile(),
+                        member.getDeleteStatus()
                 ))
                 .collect(Collectors.toList());
     }
@@ -42,7 +42,8 @@ public class SearchService {
                         member.getId(),
                         member.getUsername(),
                         member.getEmail(),
-                        member.getProfile()
+                        member.getProfile(),
+                        member.getDeleteStatus()
                 ))
                 .collect(Collectors.toList());
     }
@@ -55,7 +56,8 @@ public class SearchService {
                         project.getId(),
                         project.getName(),
                         project.getCreator().getUsername(),
-                        project.getStatus()
+                        project.getStatus(),
+                        project.getDeleteStatus()
                 ))
                 .collect(Collectors.toList());
     }
