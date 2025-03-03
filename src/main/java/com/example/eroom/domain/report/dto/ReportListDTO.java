a3fe8274e4d3ca4b57dd0cc5e6fdb4b7d80ec58b
joybully <joybully@khu.ac.kr>
2025-03-03 18:09:35 +0900

    fix: 에러있는 코드 수정(reportListDTO의 @Builder부분)

--- src/main/java/com/example/eroom/domain/report/dto/ReportListDTO.java
+++ src/main/java/com/example/eroom/domain/report/dto/ReportListDTO.java
@@ -1,6 +1,8 @@
 package com.example.eroom.domain.report.dto;
 
 import com.example.eroom.domain.entity.DeleteStatus;
+import com.example.eroom.domain.entity.Report;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,8 +10,20 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
-@Builder
 public class ReportListDTO {
+
+    @Builder
+    ReportListDTO(Long id, String content, Long chatRoomId, LocalDateTime createdAt, LocalDateTime startDate, LocalDateTime endDate, String title, DeleteStatus deleteStatus, List<String> members) {
+        this.id = id;
+        this.content = content;
+        this.chatRoomId = chatRoomId;
+        this.createdAt = createdAt;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.deleteStatus = deleteStatus;
+        this.members = members;
+    }
     private Long id;
 
     private String content;
