522f58c4ef5197091fb80e1a3e8fcb189502da9d
suho <yousuho1232@naver.com>
2025-03-03 12:03:10 +0900

    feat : 멤버, 프로젝트 검색 응답 DTO에 추가 필드 설정, LocalDateTime 시간대를 서울로 설정

--- src/main/java/com/example/eroom/domain/chat/dto/response/ProjectSearchResponseDTO.java
+++ src/main/java/com/example/eroom/domain/chat/dto/response/ProjectSearchResponseDTO.java
@@ -6,14 +6,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectSearchResponseDTO {
 
-    private Long id;
-    private String name;
-    private String creator;
-    private ProjectStatus status;
+    private Long projectId;
+    private String projectName;
+    private String creatorName;
+    private String creatorEmail;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private LocalDateTime createdAt;
+    private ProjectStatus projectStatus;
     private DeleteStatus deleteStatus;
 }
