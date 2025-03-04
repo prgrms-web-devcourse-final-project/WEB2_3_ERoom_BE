dec2fe968608645e5edc36fadeed32236c195a65
suho <yousuho1232@naver.com>
2025-03-04 17:09:21 +0900

    refactor : 토큰 정보를 헤더에 포함하도록 수정

--- src/main/java/com/example/eroom/domain/chat/interceptor/WebSocketAuthInterceptor.java
+++ src/main/java/com/example/eroom/domain/chat/interceptor/WebSocketAuthInterceptor.java
@@ -2,6 +2,7 @@ package com.example.eroom.domain.chat.interceptor;
 
 import com.example.eroom.domain.auth.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
@@ -16,16 +17,23 @@ public class WebSocketAuthInterceptor implements HandshakeInterceptor {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Override
-    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) {
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response,
+                                   WebSocketHandler wsHandler, Map<String, Object> attributes) {
         if (request instanceof ServletServerHttpRequest servletRequest) {
-            String token = servletRequest.getServletRequest().getParameter("token"); // url 파라미터에서 토큰 가져오기
-            if (token != null && jwtTokenProvider.validateToken(token)) {
-                attributes.put("member", jwtTokenProvider.getAuthentication(token));
-                return true;
+            HttpHeaders headers = servletRequest.getHeaders();
+            String authHeader = headers.getFirst("Authorization");
+
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String token = authHeader.substring(7);
+                if (jwtTokenProvider.validateToken(token)) {
+                    attributes.put("member", jwtTokenProvider.getAuthentication(token));
+                    return true;
+                }
             }
         }
         return false;
     }
+
 
     @Override
     public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
