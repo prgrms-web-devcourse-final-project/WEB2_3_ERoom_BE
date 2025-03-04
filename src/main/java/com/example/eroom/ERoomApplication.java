3c8c9219139d3e86098df236654f6cba127b67d1
joybully <joybully@khu.ac.kr>
2025-03-04 11:37:36 +0900

    feat: 서버시간대 한국을 기준으로 변경

--- src/main/java/com/example/eroom/ERoomApplication.java
+++ src/main/java/com/example/eroom/ERoomApplication.java
@@ -1,9 +1,12 @@
 package com.example.eroom;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -11,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ERoomApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(ERoomApplication.class, args);
     }
 
