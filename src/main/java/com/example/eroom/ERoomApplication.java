5f4f63857d8ae967124e5535ea248e7f3abd9d57
joybully <joybully@khu.ac.kr>
2025-03-05 12:54:26 +0900

    fix: notification/memberId 경로로 구독하는 알림이 빈 배열이 와서 notification객체를 넣음

--- src/main/java/com/example/eroom/ERoomApplication.java
+++ src/main/java/com/example/eroom/ERoomApplication.java
@@ -1,9 +1,14 @@
 package com.example.eroom;
 
+import com.example.eroom.domain.chat.repository.NotificationRepository;
+import com.example.eroom.domain.chat.service.NotificationService;
+import com.example.eroom.domain.entity.Member;
+import com.example.eroom.domain.entity.NotificationType;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
