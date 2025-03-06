6fed79c7d93cfd82f5364a882803a76557a32024
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-03-06 14:40:44 +0900

    feat: @EnableElasticsearchRepositories 어노테이션 추가

--- src/main/java/com/example/eroom/ERoomApplication.java
+++ src/main/java/com/example/eroom/ERoomApplication.java
@@ -7,6 +7,7 @@ import com.example.eroom.domain.entity.NotificationType;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,6 +16,7 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableElasticsearchRepositories(basePackages = "com.example.eroom.domain.elasticsearch.repository")
 //@EnableJpaAuditing
 public class ERoomApplication {
 
