69fb32947d20ab5d6ddd3169631c5ff471124a23
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2025-03-06 23:47:59 +0900

    fix: 관리자 ) 계정 수정 시 응답 데이터 오류 수정

--- src/main/java/com/example/eroom/domain/admin/dto/request/AdminUpdateMemberDTO.java
+++ src/main/java/com/example/eroom/domain/admin/dto/request/AdminUpdateMemberDTO.java
@@ -1,22 +1,16 @@
 package com.example.eroom.domain.admin.dto.request;
 
-import com.example.eroom.domain.entity.MemberGrade;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.time.LocalDate;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminUpdateMemberDTO {
-    private String username;
-    private LocalDate createdAt;
-    private MemberGrade memberGrade;
-    private String organization;
-    private String profile;
+    private String name;
 
+    public void setName(String name){
+        this.name = name;
+    }
 }
